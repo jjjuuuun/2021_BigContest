1c9635e48c894fda652d1f3f0708e8eb4395ba02
LAPTOP-2MQAB4GC\82102 <kkakkuro0@gmail.com>
2021-08-25 13:51:19 +0900

    no message

--- barrel.py
+++ barrel.py
@@ -2,15 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-from sklearn.cluster import DBSCAN
-import seaborn as sns
-from sklearn.preprocessing import StandardScaler
-from scipy.cluster.hierarchy import linkage, dendrogram, ward
-import mglearn
 from sklearn.cluster import AgglomerativeClustering
-# =============================================================================
-# import scipy.cluster.hierarchy as shc
-# =============================================================================
 os.chdir('C:/Users/82102/Desktop/bigcon/01_제공데이터')
 
 #분활된 HTS데이터를 읽어와요
@@ -19,13 +11,13 @@ HTS_two = pd.read_csv('hts_2.csv')
 HTS_three = pd.read_csv('hts_3.csv')
 HTS_four = pd.read_csv('hts_4.csv')
 split_HTS = [HTS_one,HTS_two,HTS_three,HTS_four]
-
+#=============================함수========================================
 #Agglormerative clustering을 하는 함수에요
 def clustering(dataset):
     sam_data = dataset.iloc[:, 1:3].values
     agg_clustering = AgglomerativeClustering(n_clusters=500, linkage='average')
     labels = agg_clustering.fit_predict(sam_data)
-    oncat([dataset,pd.DataFrame(labels)],axis = 1)
+    dataset = pd.concat([dataset,pd.DataFrame(labels)],axis = 1)
     dataset.rename(columns={0:'label'},inplace=True)
     return dataset
 
@@ -34,7 +26,6 @@ def barrel_selection(dataset):
     
     barrel_df = pd.DataFrame()
     hit_list = [1,2,3,4,7,15]
-    out_list = [5,6,7,8,9,10,11,12,13,14]
     #-----------------------------------------------------------------------
     #이 씹새끼가 아주 문제에요
     #각 타구의 타율과 장타율을 계산해요
@@ -76,15 +67,40 @@ def barrel_selection(dataset):
             real_barrel_df = real_barrel_df.append(HTS_one.loc[i], ignore_index=True)
 
     return real_barrel_df
+#=============================================================================
 
 #클러스터링을 실행하고
 #타구들을 선별해요
 zzin_barrel_df = pd.DataFrame()
 for i in range(len(split_HTS)):
     split_HTS[i] = clustering(split_HTS[i])
-    zzin_barrel_df = pd.cancat[zzin_barrel_df,barrel_selection(split_HTS[i])]
+    zzin_barrel_df = pd.concat[zzin_barrel_df,barrel_selection(split_HTS[i])]
 
 #선별된 타구들의 산점도를 찍어봐요 ㅎㅎ
 scatter_plot = zzin_barrel_df.plot.scatter(x='HIT_VEL',y='HIT_ANG_VER')
-scatter_plot.plot()
+scatter_plot.plot() 
 plt.show()
+
+for i in range(0,500):
+    new_df = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
